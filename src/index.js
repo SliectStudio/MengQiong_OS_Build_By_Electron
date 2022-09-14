b83e551441bae8f22a41c64969c58a9126667ad6
HuYihe2008 <2812856215@qq.com>
2022-09-14 14:47:50 +0800

    v1.0.0a108

---
+++ src/index.js
@@ -0,0 +1,101 @@
+const { app, BrowserWindow, dialog } = require('electron');
+const { NsisUpdater } = require("electron-updater");
+const path = require('path');
+const { autoUpdater } = require('electron-updater')
+function update() {
+  
+        const options = {
+            provider: 'generic',
+            url: `http://mengqiongos.sliect.top/${process.platform}`
+        }
+
+  const autoUpdater = new NsisUpdater(options)
+  console.log(options)
+  //检测更新
+  autoUpdater.checkForUpdatesAndNotify()
+        
+  autoDownload = true
+  autoUpdater.on('update-available', () => {
+    console.log('find a new pak'),
+    dialog.showMessageBox({
+      message: '找到了新的版本'
+    })
+  })
+
+  autoUpdater.on('update-not-available', () =>
+    dialog.showMessageBox({
+    message:'您已是最新版'
+  })
+  )
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: '应用更新',
+      message: '发现新版本，是否立即更新？若选择否则在关闭程序后自动更新！',
+      buttons: ['是', '否']
+    }).then((buttonIndex) => {
+      if(buttonIndex.response == 0) {  //选择是，则退出程序，安装新版本
+        autoUpdater.quitAndInstall() 
+        app.quit()
+      }else{
+        dialog.showMessageBox({
+            message: '应用将在关闭程序后更新！'
+          })
+      }
+    })
+  })
+
+}
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// eslint-disable-next-line global-require
+/*if (require('electron-squirrel-startup')) {
+  app.quit();
+}*/
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  //每次启动程序，就检查更新
+  update()
+});
+
+app.on('ready', createWindow)
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
