c83bcd50a5abebf686e02494ba5e97ac7e979883
HuYihe2008 <2812856215@qq.com>
2022-09-19 22:32:08 +0800

    v1.0.0-beta.1

--- src/mian/index.js
+++ src/mian/index.js
@@ -6,7 +6,7 @@ function update() {
   
         const options = {
             provider: 'generic',
-            url: `http://mengqiongos.sliect.top/${process.platform}`
+            url: `http://test.mengqiongos.sliect.top/${process.platform}`
         }
 
   const autoUpdater = new NsisUpdater(options)
---
+++ src/render/assets/index.js
@@ -0,0 +1,3 @@
+$(function() {
+    $('#spinner').fadeOut(5000);
+  })
